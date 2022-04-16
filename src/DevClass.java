387f86005dc266100ee80e8c79a9b4154e81b30c
pragati <pragatipadhi8@gmail.com>
2022-04-16 15:50:24 +0530

    String conversion to upper case

--- src/DevClass.java
+++ src/DevClass.java
@@ -1,8 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DevClass {
-    private static String myMessage = "Update Successful";
+    private static String myMessage = "Updated Successfully";
+    //private static List<String> strList = Arrays.asList("adam","john","lilly");
 
     public static String printMessage(String message){
-        return message;
+
+        String msg = message.toUpperCase();
+        /*String result = strList.stream()
+                .collect(Collectors.joining());*/
+
+        return msg;
     }
     public static void main(String[] args) {
         System.out.println("This is a new class on Dev Branch");
