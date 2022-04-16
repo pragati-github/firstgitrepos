2a53a0801cea5914c9296e5a0116bfa5e846a608
pragati <pragatipadhi8@gmail.com>
2022-04-16 15:46:30 +0530

    fixed the bug

--- src/DevClass.java
+++ src/DevClass.java
@@ -1,11 +1,22 @@
-public class DevClass {
-    private static String myMessage = "Update Successful";
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
-    public static String printMessage(String message){
-        return message;
+public class DevClass {
+    //private static String myMessage = "Update Successful";
+    private static List<String> strList = Arrays.asList("adam","john","lilly");
+
+    public static int printMessage(List<String> strList){
+
+        //String msg = message.toUpperCase();
+        String result = strList.stream()
+                .collect(Collectors.joining());
+
+        return result.length();
     }
     public static void main(String[] args) {
         System.out.println("This is a new class on Dev Branch");
-        System.out.println(printMessage(myMessage));
+        System.out.println("The total number of characters in list :"+printMessage(strList));
     }
 }
