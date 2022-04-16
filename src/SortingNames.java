58210f6cb3602b6eb733aefd569aea2664e090f5
pragati <pragatipadhi8@gmail.com>
2022-04-16 11:47:41 +0530

    Added multiple names to sort in reverse order

--- src/SortingNames.java
+++ src/SortingNames.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class SortingNames {
     public static void main(String[] args) {
-        List<String> strList = Arrays.asList("Pragati","Dibya","Barsha");
+        List<String> strList = Arrays.asList("Aavya","Sonu","Dibya","Barsha","Pragati");
         List<String> result = strList.stream()
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
