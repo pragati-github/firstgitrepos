cb1dc0b105f09dcb88b5968b71a382af0cf509a2
pragati <pragatipadhi8@gmail.com>
2022-04-16 16:16:52 +0530

    Updated bugfix

--- src/DevClass.java
+++ src/DevClass.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -18,5 +19,7 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("This is a new class on Dev Branch");
         System.out.println("The total number of characters in list :"+printMessage(strList));
+        strList.sort(Comparator.reverseOrder());
+        strList.forEach(s -> System.out.println(s));
     }
 }
