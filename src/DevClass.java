c93a145e0bd1cfc9c61a2a9bb45f3e92f4a5a2a5
pragati <pragatipadhi8@gmail.com>
2022-04-16 15:31:43 +0530

    added new method printMessage

--- src/DevClass.java
+++ src/DevClass.java
@@ -1,5 +1,11 @@
 public class DevClass {
+    private static String myMessage = "Update Successful";
+
+    public static String printMessage(String message){
+        return message;
+    }
     public static void main(String[] args) {
         System.out.println("This is a new class on Dev Branch");
+        System.out.println(printMessage(myMessage));
     }
 }
