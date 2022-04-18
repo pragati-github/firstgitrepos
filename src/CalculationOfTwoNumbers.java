e2820172169b308c71da046751e00a1cc0248a94
pragati <pragatipadhi8@gmail.com>
2022-04-18 20:13:50 +0530

    added method division

--- src/CalculationOfTwoNumbers.java
+++ src/CalculationOfTwoNumbers.java
@@ -5,8 +5,17 @@ public class CalculationOfTwoNumbers {
     public static String multiplication(int a,int b){
         return "The multiplication of two numbers is : "+(a*b);
     }
+    public static String division(int a,int b){
+        try {
+            return "The division of two numbers is : "+(a/b);
+        }
+        catch (ArithmeticException e){
+            throw new ArithmeticException("Please provide a number which is greater than 0");
+        }
+    }
     public static void main(String[] args) {
         System.out.println(addition(100,200));
         System.out.println(multiplication(10,20));
+        System.out.println(division(10,5));
     }
 }
