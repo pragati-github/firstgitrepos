09cda985025997672d2c73eb5715ad3e2fbe894b
pragati <pragatipadhi8@gmail.com>
2022-04-18 19:54:29 +0530

    added another method on CalculationOfTwoNumbers

--- src/CalculationOfTwoNumbers.java
+++ src/CalculationOfTwoNumbers.java
@@ -1,8 +1,12 @@
 public class CalculationOfTwoNumbers {
-    public static String add(int a ,int b){
+    public static String addition(int a ,int b){
         return "The addition of two numbers is : "+(a+b);
     }
+    public static String multiplication(int a,int b){
+        return "The multiplication of two numbers is : "+(a*b);
+    }
     public static void main(String[] args) {
-        add(100,200);
+        System.out.println(addition(100,200));
+        System.out.println(multiplication(10,20));
     }
 }
